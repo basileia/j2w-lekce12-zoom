4196c7607f1e1496dc30b01bd4a3efdb6690ad44
basileia <jane.novak@centrum.cz>
2022-12-12 18:55:00 +0100

    Výchozí stav projektu

---
+++ src/main/java/cz/czechitas/java2webapps/lekce12/J2wLekceZoomApplication.java
@@ -0,0 +1,13 @@
+package cz.czechitas.java2webapps.lekce12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J2wLekceZoomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(J2wLekceZoomApplication.class, args);
+    }
+
+}
